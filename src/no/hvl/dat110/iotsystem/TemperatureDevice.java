0316619a58bc965cdd2ebb215d3ff431d7334dd9
h577860 <577860@stud.hvl.no>
2020-03-07 19:33:28 +0100

    Merge IoTsystem changes

--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -3,6 +3,8 @@ package no.hvl.dat110.iotsystem;
 import no.hvl.dat110.client.Client;
 import no.hvl.dat110.common.TODO;
 
+import static java.lang.Thread.sleep;
+
 public class TemperatureDevice {
 
 	private static final int COUNT = 10;
@@ -12,6 +14,21 @@ public class TemperatureDevice {
 		// simulated / virtual temperature sensor
 		TemperatureSensor sn = new TemperatureSensor();
 
+		Client client = new Client("temperature sensor", Common.BROKERHOST,Common.BROKERPORT);
+		
+		client.connect();
+		
+		
+        for (int i = 0; i < COUNT; i++) {
+            try {
+                client.publish(Common.TEMPTOPIC, Integer.toString(sn.read()));
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+		
+		client.disconnect();
 		// TODO - start
 
 		// create a client object and use it to
@@ -24,7 +41,7 @@ public class TemperatureDevice {
 
 		System.out.println("Temperature device stopping ... ");
 
-		throw new UnsupportedOperationException(TODO.method());
+		// throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
